38abfeed2562e6e1c41d22b1890d2fb8eb370819
LeonFSchaller <161484069+LeonFSchaller@users.noreply.github.com>
2024-10-08 13:55:41 +0200

    Fixed partial circle bug

--- code/api/drawing_bot_api/shapes.py
+++ code/api/drawing_bot_api/shapes.py
@@ -59,10 +59,11 @@ class Partial_circle:
 
         center_to_start_vector = [self.start_point[0]-self.center_point[0], self.start_point[1]-self.center_point[1]]
         self.offset = atan2(center_to_start_vector[1], center_to_start_vector[0])
+        print(self.offset)
 
     def get_point(self, t):
-        x = self.radius * cos(self.offset + t * self.section_angle) + self.center_point[0]
-        y = self.radius * sin(self.offset + t * self.section_angle) + self.center_point[1]
+        x = self.radius * cos(self.offset + (t * self.direction * self.section_angle)) + self.center_point[0]
+        y = self.radius * sin(self.offset + (t * self.direction * self.section_angle)) + self.center_point[1]
         return [x, y]
     
     def plot(self):
--- code/api/drawing_test.py
+++ code/api/drawing_test.py
@@ -13,9 +13,12 @@ drawing_bot = Drawing_Bot()
 def main():
     #while not drawing_bot.is_ready():
         #time.sleep(0.5)
-
     #while(1):
-    drawing_bot.add_shape(shapes.Circle([X0, Y0], R))
+    drawing_bot.add_shape(shapes.Line([-50, 80], [50, 80]))
+    drawing_bot.add_shape(shapes.Partial_circle([50, 80], [50, 50], 15, -1))
+    drawing_bot.add_shape(shapes.Line([50, 50], [-50, 50]))
+    drawing_bot.add_shape(shapes.Partial_circle([-50, 50], [-50, 20], 15, 1))
+    drawing_bot.add_shape(shapes.Line([-50, 20], [0, 20]))
     drawing_bot.plot()
     #drawing_bot.execute()
     
